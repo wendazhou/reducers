269ae17a9f7c1ae31fb9c69b77494a2392c36893
Wenda Zhou <zhouwenda@outlook.com>
2013-09-27 12:06:09 +0200

    add global include file

---
+++ reducers/include/wenda/reducers.h
@@ -0,0 +1,20 @@
+#ifndef WENDA_REDUCERS_H_INCLUDED
+#define WENDA_REDUCERS_H_INCLUDED
+
+// this file includes all the headers for the wenda/reducers library
+
+#include "reducers/reducers_common.h"
+#include "reducers/reduce.h"
+
+#include "reducers/range_reducible.h"
+#include "reducers/iterator_pair_reducible.h"
+
+#include "reducers/collect.h"
+#include "reducers/filter.h"
+#include "reducers/map.h"
+
+#include "reducers/into.h"
+
+#include "reducers/sequence_reducible.h"
+
+#endif // WENDA_REDUCERS_H_INCLUDED
